9e8816cf2c041f24da6c743f92fd169d17936220
colonistz-1 <tony.nguyen2415@gmail.com>
2024-12-09 00:30:27 -0800

    code compiles

--- app/src/App.java
+++ app/src/App.java
@@ -1,12 +1,58 @@
-import java.io.IOException;  
-import javax.swing.text.html.parser.Element;
-import org.jsoup.Jsoup;  
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-public class App{  
-    public static void main( String[] args ) throws IOException{  
-                Document doc = Jsoup.connect("https://www.amazon.com/Bluetooth-Belt-Driven-Turntable-Speakers-Headphone/dp/B07N3WYLKZ?crid=1JK35IVQNR3L2&dib=eyJ2IjoiMSJ9.cxFMAS42Ep4jY1NvUOT9XqkNfs1jsT9zd3txfyjncJVUj-W-s6TBlqDWEPkCLLV79wLgvR70MPAj9d3jdTtgUzS5Cbwdj85DtStE5rFNI_IHbTwf6k7xz1J_1qy2-m_6yvwDkYn9g0dgGSG95Q1SFtc_0VsBR929Pga8gXExp4b5rnx8tQpw8b6SbUQqh6D59yCMFjwfUdYg_SsXrTXBkFHf-S6-ARxy22i0mzTrkng.2bR75VTU5IN4wbu7UHsjMsPAk3V6qgNW2fTJ3Yv3Imo&dib_tag=se&keywords=vinyl%2Brecord%2Bplayer%2Bvintage&qid=1733644845&sprefix=vinyl%2Brecord%2Bplayer%2Bvint%2Caps%2C248&sr=8-3&th=1").get();  
-                String title = doc.title();  
-                System.out.println("title is: " + title);
-    }  
-}  
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        List<Review> reviews = new ArrayList<>();
+   
+        String url = "https://www.amazon.com/Orange-Mushroom-Lamp-Dimmable-Mid-Century/dp/B0CP6VVKMH?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&smid=A2REG95GY1M7ED&th=1";
+
+        Document doc = Jsoup.connect(url).get();
+        Elements reviewElements = doc.select(".review");
+
+        for (Element reviewElement : reviewElements) {
+
+            Element titleElement = reviewElement.select(".review-title").first();
+            String title = titleElement.text();
+
+            Element textElement = reviewElement.select(".review-text").first();
+            String text = textElement.text();
+
+                reviews.add(new Review(title, text));
+            }
+
+        for (Review review : reviews) {
+            System.out.println(review.getTitle());
+            System.out.println(review.getText());
+            System.out.println("\n");
+        }
+    }
+
+    static class Review {
+
+        private final String title;
+        private final String text;
+
+        public Review(String title, String text) {
+            this.title = title;
+            this.text = text;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+    }
+
+}
